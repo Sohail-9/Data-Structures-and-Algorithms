d05d25e71cfa66cd231bdb21324ca5b97a05f9c2
Sohail-9 <s.suhail9849@gmail.com>
2024-05-18 18:25:30 +0530

    permutation problem

---
+++ Arrays/31. Next Permutation.cc
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // inbuilt function for next permuatation in c++
+        next_permutation(nums.begin(), nums.end());
+    }
+};
