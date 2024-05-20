bedeb05556d05b37f4d16293b4685c8b3da382a9
Sohail-9 <s.suhail9849@gmail.com>
2024-05-20 18:28:09 +0530

    new problem

---
+++ Binary Search/34. Find First and Last Position of Element in Sorted Array.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //using lower bound and upper bound
+        int l=lower_bound(nums.begin() , nums.end() , target)-nums.begin();
+        int u= upper_bound(nums.begin() , nums.end() , target)-nums.begin()-1;
+        if(l==nums.size() || nums[l] !=target)
+            return {-1,-1};
+        return {l,u};
+    }
+   
+};
