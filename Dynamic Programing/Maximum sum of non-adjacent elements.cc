084e7f0c7e165d4cd856ee43418d4ab9a6d58e34
Sohail-9 <s.suhail9849@gmail.com>
2024-05-29 03:46:58 +0530

    max nums

---
+++ Dynamic Programing/Maximum sum of non-adjacent elements.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+int maximumNonAdjacentSum(vector<int> &nums){
+    //space complexity optimization
+    int n = nums.size();
+    int prev = nums[0];
+    int prev2 = 0;
+    for(int i=1;i<n;++i){
+        int take = nums[i];
+        if(i>1) take+= prev2;
+        int notTake = 0 +prev;
+        int curi = max(take, notTake);
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
+}
